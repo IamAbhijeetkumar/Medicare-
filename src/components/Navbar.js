3b76248f3e84c293beb7e276f69a268d1d8c65d0
Abhijeet kumar <iamabhijeetkumar17@gmail.com>
2023-01-29 11:49:07 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Navbar = () => {
- 
-    const [showmenu , setShowmenu] = useState(false);
-    return (
-        <div>
-            <nav className='Navbar'>
-                <div className='logo'>
-                    <h1>Medi<span>Care+</span></h1>
-                </div>
-                <div className={showmenu ? "menu-list mobile-menu":"mobile-menu"}>
-                    <ul>
-                        <li><a href='/home'>Home</a></li>
-                        <li><a href='/about'>About</a></li>
-                        <li><a href='/service'>Service</a></li>
-                        <li><a href='/news'>News</a></li>
-                    </ul>
-                </div>
-                <button className={showmenu ? "button btn5":"btn5"}>Contact</button>
-                <div onClick={()=> setShowmenu(!showmenu)} className='burger'>
-                    <i class="fa-solid fa-bars"></i>
-                </div>
-                <div className='Oval'></div>
-                <div className='Rectangle'></div>
-                <div className='Welcome-Message'>
-                    <h5>Welcome to MediCare+ Clinic</h5>
-                    <h1>Best Specialists</h1>
-                    <h6>We are on the leading edge of cancer care. Providing the full continuum of cancer treatments and supportive care services in a single convenient location.</h6>
-                    <button className='button1'>Make an Appointment</button>
-                    <button className='button2'>Departments</button>
-                </div>
-
-
-            </nav>
+  const [showmenu, setShowmenu] = useState(true);
+  return (
+    <div>
+      <nav className="Navbar">
+        <div className="logo">
+          <h1>
+            Medi<span>Care+</span>
+          </h1>
         </div>
-    )
-}
+        <div className={showmenu ? "menu-list mobile-menu" : "mobile-menu"}>
+          <ul>
+            <li>
+              <a href="/home">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/service">Service</a>
+            </li>
+            <li>
+              <a href="/news">News</a>
+            </li>
+          </ul>
+        </div>
+        <button className={showmenu ? "button btn5" : "btn5"}>Contact</button>
+        <div onClick={() => setShowmenu(!showmenu)} className="burger">
+          <i class="fa-solid fa-bars"></i>
+        </div>
+        <div className="Oval"></div>
+        <div className="Rectangle"></div>
+        <div className="Welcome-Message">
+          <h5>Welcome to MediCare+ Clinic</h5>
+          <h1>Best Specialists</h1>
+          <h6>
+            We are on the leading edge of cancer care. Providing the full
+            continuum of cancer treatments and supportive care services in a
+            single convenient location.
+          </h6>
+          <button className="button1">Make an Appointment</button>
+          <button className="button2">Departments</button>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
