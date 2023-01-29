b1ba20507187865c5761dab61e73351afb2743fc
Abhijeet kumar <118126044+IamAbhijeetkumar@users.noreply.github.com>
2023-01-29 17:09:41 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
                 <div className='logo'>
                     <h1>Medi<span>Care+</span></h1>
                 </div>
-                <div className={showmenu ? "menu-list mobile-menu":"mobile-menu"}>
+                <div className={showmenu ? "mobile-menu menu-list":"mobile-menu"}>
                     <ul>
                         <li><a href='/home'>Home</a></li>
                         <li><a href='/about'>About</a></li>
